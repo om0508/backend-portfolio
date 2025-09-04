ab365e6f534f573e534a79f9ea9bf24dbfee4ad6
Om Raut <95272219+om0508@users.noreply.github.com>
2025-09-04 13:36:48 +0530

    Create restart_server.sh

---
+++ restart_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Stop any existing Node.js process
+pkill node
+
+# Go to backend folder
+cd /home/ubuntu/backend
+
+# Install dependencies
+npm install
+
+# Start backend in background
+nohup node app.js > app.log 2>&1 &
